31aea7de3a5ee22941477f9065bc4bd6641aa8fd
osama2020almorisi <2025ooss@gmail.com>
2026-09-21 18:37:30 +0300

    تحديث شامل

---
+++ projects/tree/sw.js
@@ -0,0 +1,39 @@
+const CACHE = 'familytree-v1';
+const FILES = [
+  './',
+  './index.html',
+  './tree-data.json',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES).catch(() => {}))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+      return fetch(e.request).then(response => {
+        if (response && response.status === 200) {
+          const clone = response.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return response;
+      }).catch(() => cached);
+    })
+  );
+});
